8181c4193b3bd06d233cc7361875531cfd542e48
Zeista01 <150901226+Zeista01@users.noreply.github.com>
2025-02-23 04:12:46 +0530

    Update questions1.js to land at endpoint

--- public/assets/scripts/questions1.js
+++ public/assets/scripts/questions1.js
@@ -92,7 +92,7 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function findMatch() {
-    fetch('https://anime-bae-radar-git-main-zans-projects-9183aca1.vercel.app/findMatch', {
+    fetch('https://anime-bae-radar-git-main-zans-projects-9183aca1.vercel.app', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
